a6a86d320405ee969db691ff33dfe76797d6f11e
Nikodim-monk <lr1975@yandex.ru>
2022-09-30 17:17:45 +0400

    надеюсь - готово

--- src/main/java/ru/kata/spring_boot_security/entity/Role.java
+++ src/main/java/ru/kata/spring_boot_security/entity/Role.java
@@ -2,17 +2,32 @@ package ru.kata.spring_boot_security.entity;
 
 import lombok.*;
 import javax.persistence.*;
+import java.util.Objects;
+
 @Entity
-@Table(name = "roles")
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role1 = (Role) o;
+        return id == role1.id && role.equals(role1.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
 }
 
--- src/main/java/ru/kata/spring_boot_security/entity/User.java
+++ src/main/java/ru/kata/spring_boot_security/entity/User.java
@@ -3,11 +3,13 @@ package ru.kata.spring_boot_security.entity;
 import lombok.*;
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.Objects;
+
 @Entity
-@Table(name = "users")
 @Getter
 @Setter
 @ToString
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +32,20 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Collection<Role> roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && firstName.equals(user.firstName) &&
+                lastName.equals(user.lastName) && email.equals(user.email) &&
+                password.equals(user.password) && roles.equals(user.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, age, email, password, roles);
+    }
 }
+
