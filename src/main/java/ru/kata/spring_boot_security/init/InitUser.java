8c2a6d678b71be8d1e41fc261ccee13eab41b649
Nikodim-monk <lr1975@yandex.ru>
2022-10-05 17:59:59 +0400

    надеюсь - готово

--- src/main/java/ru/kata/spring_boot_security/init/InitUser.java
+++ src/main/java/ru/kata/spring_boot_security/init/InitUser.java
@@ -1,13 +1,14 @@
 package ru.kata.spring_boot_security.init;
 
 import ru.kata.spring_boot_security.entity.User;
-
 public class InitUser {
+
     public static User createDefaultUserInBase() {
-        return new User("USER","USEROV",100,"user@mail.ru","user");
+        return new User("User","Userov",100,"user@mail.ru","user");
+
     }
     public static User createDefaultAdminInBase() {
-        return new User("ADMIN","ADMINOV",100,"admin@mail.ru","admin");
+        return new User("Admin","Adminov",100,"admin@mail.ru","admin");
     }
 }
 
